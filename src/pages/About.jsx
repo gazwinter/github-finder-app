02e96b699751b8ae6e4b4290b79b407bf628aab3
Gareth Winter <garethwinter@gmail.com>
2022-06-08 14:17:24 +0100

    Final version of github-finder-app from Udemy course. Removed .env file so as not to expose API Key

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+function About() {
+    return(
+        <div>
+            <h1 className="text-6xl mb-4">Github Finder</h1>
+            <p className="mb-4 text-2xl font-light">
+                A React app to search GitHub profiles and see their details. This is part of a Udemy Course called React Front To Back by <strong>Brad Traversy</strong>
+            </p>
+            <p className="text-lg text-gray-400">
+                Version <span className="text-white">1.0.0</span>
+            </p>
+            <p className="text-lg text-gray-400">
+                Layout by: Hassib Moddasser
+            </p>
+        </div>
+    )
+}
+
+export default About;
